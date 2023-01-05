36b88fb40b2352d682eb23f8c08b1795b1da3ca7
Matt Laguardia <matthewlaguardia@gmail.com>
2023-01-05 14:02:08 -0800

    feat: demo for card animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react';
+import { FC, useEffect, useState, useRef } from 'react';
+import autoAnimate from '@formkit/auto-animate';
 import './App.css'
 
 import { CountProvider } from './providers';
@@ -7,13 +8,47 @@ import ComponentOne from './components/ComponentOne';
 import ComponentTwo from './components/ComponentTwo';
 
 const App: FC = () => {
+  const images = new Array(32)
+    .fill('')
+    .map((el, index) => {
+      return {
+        id: index,
+      }
+    });
+
+  const parent = useRef(null)
+
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current)
+  }, [parent])
+
+  const [ imagesState, setImagesState ] = useState(images);
+
   return (
     <div className="App">
       <div className="card">
+        <h3>Shared State Example Implementation</h3>
         <CountProvider>
           <ComponentOne />
           <ComponentTwo />
         </CountProvider>
+      </div>
+
+      <div className="card">
+        <h3>Card Animation</h3>
+        <div className="container" ref={parent}>
+          {
+            imagesState.map((selector) =>
+              <div className="image-card"
+                key={selector.id}
+                onClick={() => {
+                  setImagesState(imagesState.filter(el => el.id !== selector.id));
+                }}
+              >image { selector.id }</div>
+            )
+          }
+        </div>
+
       </div>
     </div>
   )
